ec3ec981217cb85160579866abdba82475fa35a6
faggioC <90908062+faggioC@users.noreply.github.com>
2021-11-25 10:15:56 +0100

    Add files via upload

--- Python/esercizio30.py
+++ Python/esercizio30.py
@@ -1,6 +1,7 @@
+isMaiuscola = lambda s: True if s[0].isupper() else False
+
 s = input("Inserisci una stringa: ")
-c = True if s[0].isupper() else False
-if c == True:
+if isMaiuscola(s) == True:
     print("La prima lettera è maiuscola")
 else:
     print("La prima lettera non è maiuscola")
